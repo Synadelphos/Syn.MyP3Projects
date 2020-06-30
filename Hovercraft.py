fe28c7c6b1a7de4456437af28433b9c50b28fc8c
Synadelphos <adamsj1826@gmail.com>
2020-06-30 15:47:59 -0700

    added commits

--- Hovercraft.py
+++ Hovercraft.py
@@ -1,15 +1,18 @@
+# get user input
 u_input = int(input())
 
 
+# define net profit
 def hovercrafts(x=u_input):
 	y = 1000000
 	n = x * (y * 3) - ((y * 2) * 10 + y)
 	return int(n)
 
 
+# adding profit to a var
 net = hovercrafts()
 
-
+# determine meaning of profit
 def h_outputs(x=net):
 	if x > 0:
 		return "Profit"
@@ -19,7 +22,8 @@ def h_outputs(x=net):
 		return "Loss"
 
 
+# save profit to a var
 final = h_outputs()
 
-
+# print the output
 print(final)
